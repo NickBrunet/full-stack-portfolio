9cb3f4df3084e23b6c4b5fdd0864987cd4f7f636
Nick <nickjbrunet@gmail.com>
2025-07-31 14:21:46 -0600

    Github Pages Fix

--- react-front-end/vite.config.js
+++ react-front-end/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: "/full-stack-portfolio/",
     plugins: [react()],
-    //server: {
-    //    proxy: {
-    //        '/api': {
-    //            target: 'http://localhost:5000',
-    //            changeOrigin: true,
-    //            secure: false,
-    //        },
-    //    },
-    //},
 });
