2acac452ed7ce455a837b713fb349d7e00a94cea
Nick <nickjbrunet@gmail.com>
2025-07-31 14:34:39 -0600

    Portfolio description

--- react-front-end/src/pages/Home.jsx
+++ react-front-end/src/pages/Home.jsx
@@ -11,7 +11,11 @@ export default function Home() {
                     <span className="cursor">|</span>
                 </h1>
                 <p className="description">
-                    
+                    Hi, I'm Nick Brunet — a software development student passionate about building elegant solutions to complex problems.
+                    With hands-on experience in Python, JavaScript, C#, Java, React, Node.js, PostgreSQL, and core web technologies like
+                    HTML and CSS, I specialize in crafting clean, user-focused applications. I thrive in collaborative environments and bring
+                    a strong sense of leadership, problem-solving, and adaptability to every project. This portfolio showcases my technical skills,
+                    development journey, and dedication to continuous growth as a programmer and teammate.
                 </p>
             </div>
         </div>
